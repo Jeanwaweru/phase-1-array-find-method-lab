f3e7fb8a401ccafa369d3880479c44e0d0d4bea8
Jean <jean.wachuka@student.moringaschool.com>
2024-04-01 21:32:18 +0300

    Array find method

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(record) {
+    // Use the find() method to search for a win
+    let win = record.find(game => game.result === "W");
+
+    //A win is found, return the year
+    if (win) {
+        return win.year;
+    } else {
+        // No win is found, return undefined
+        return undefined;
+    }
+}
+
+module.exports = {
+    superbowlWin
+};
